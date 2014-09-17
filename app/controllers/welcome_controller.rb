e0ff84a37fab28b8eb1a340f079e6c7250482116
msimcoe <msimcoe@COMcast.net>
2014-09-17 07:26:23 -0400

    additonal small changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,22 +9,10 @@ class WelcomeController < ApplicationController
   def marble_history
   end
 
-  def user_profile
-  	 
+  def user_profile  	 
   	 @marble = Marble.where(user_id: current_user.id)
   	 @comments = Comment.where(user_id: current_user.id) 
   end
 
-  # def marble
-  	 #@marbles = Marble.where(user_id: current_user.id)
-  	 # @comments = Comment.where(user_id: current_user.id)
-  	 # @user = User.where(user_id: current_user.id) 
-  	 # @marbles = Marble.where(user_id: current_user.id)
-  	 # @user = User.where(user_id: current_user.id)
-  #end
-
-
-
-
-
+  
 end
